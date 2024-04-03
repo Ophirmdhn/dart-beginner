fab67e40cca1e0dad74945f0dcc8922ff5e193bb
Ophirmdhn <ophiramadhan18@gmail.com>
2024-04-03 11:03:39 +0800

    control flow

---
+++ bin/if_else.dart
@@ -0,0 +1,14 @@
+void main() {
+  
+  String trafficLight = "Pink";
+
+  if (trafficLight == "Green") {
+    print("Go!");
+  } else if (trafficLight == "Yellow") {
+    print("GASSS!");
+  } else if (trafficLight == "Red") {
+    print("Stop!");
+  } else {
+    print("Traffic light is broken!");
+  }
+}
---
+++ bin/set.dart
@@ -0,0 +1,10 @@
+void main() {
+
+  Set data = {"ophi", "ahmad", "indra"};
+  print(data);
+  print(data.runtimeType);
+
+  Set<int> number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  print(number);
+  print(number.runtimeType);
+}
